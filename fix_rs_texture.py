47fa527deedc209cb5f2a58bdc4ff5ace9f35e99
olozhika <37537943+olozhika@users.noreply.github.com>
2023-12-27 16:54:21 +0800

    Add files via upload

---
+++ fix_rs_texture.py
@@ -0,0 +1,21 @@
+import c4d
+import redshift
+
+def main():
+    # 获取当前场景
+    doc = c4d.documents.GetActiveDocument()
+
+    # 获取场景中的所有材质
+    materials = doc.GetMaterials()
+    for currentMat in materials:
+        NodMaster = redshift.GetRSMaterialNodeMaster(currentMat)
+        rotShader = NodMaster.GetRoot()
+        nodes = rotShader.GetChildren()
+        for nod in nodes:
+            nod[c4d.REDSHIFT_SHADER_TEXTURESAMPLER_FILTER_ENABLE_TYPE] = 0  # 0表示none
+    # 更新场景
+    c4d.EventAdd()
+
+# 运行脚本
+if __name__=='__main__':
+    main()
